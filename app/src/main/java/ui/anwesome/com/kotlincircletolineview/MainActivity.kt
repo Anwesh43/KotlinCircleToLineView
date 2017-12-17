1e64d2118a978e4e5d3a3d62cf128e368ec7de5d
Anwesh43 <anweshthecool0@gmail.com>
2017-12-17 09:13:58 +0530

    using CircleToLine

--- app/src/main/java/ui/anwesome/com/kotlincircletolineview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlincircletolineview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlincircletolineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.kotlincircletoline.CircleToLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        CircleToLineView.create(this)
     }
 }
